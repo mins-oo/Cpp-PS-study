fcdf14a1ac82d45a7fef66242d098720a71b8f4f
mins-oo <140896104+mins-oo@users.noreply.github.com>
2025-11-19 19:20:11 +0900

    Add files via upload

---
+++ 다이나믹 프로그래밍/연속합.cpp
@@ -0,0 +1,42 @@
+﻿/*1912
+n개의 정수로 이루어진 수열에서 연속된 수를 선택해 가장 큰 합이 되는 경우를 구하라
+수를 선택하지 않을 수는 없음
+ex) 10, -4, 3, 1, 5, 6, -35, 12, 21, -1 에서 가장 큰 합은 12+21=33
+
+input n 입력 (1 <= n <= 100,000)
+	  n개의 정수 수열 입력 (-1,000 <= <= 1,000)
+output 가장 큰 합 출력
+
+case 1 [10 10 -4 3 1 5 6 -35 12 21 -1] [33]
+case 2 [10 2 1 -4 3 4 -4 6 5 -5 1] [14]
+case 3 [5 -1 -2 -3 -4 -5] [-1]
+*/
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int n;
+	cin >> n;
+	
+	int tmp, curSum = 0, maxSum = -99999;
+	for(int i = 0; i < n; i++){
+		cin >> tmp;
+		curSum += tmp;
+		maxSum = max(maxSum, curSum);
+		
+		if(curSum <= 0){
+			curSum = 0;
+			continue;
+		}
+	}
+	
+	cout << maxSum << endl;
+	
+	return 0;
+}
---
+++ 다이나믹 프로그래밍/파도반 수열.cpp
@@ -0,0 +1,42 @@
+﻿/*9461
+변의 길이가 1인 정삼각형 옆에 정삼각형을 계속 추가할 것임
+이때 나선에서 가장 긴 변의 길이 k인 정삼각형을 추가
+파도반 수열 P(N)은 정삼각형의 변의 길이 {1, 1, 1, 2, 2, 3, 4, 5, 7, 9}
+
+input 테스트 케이스의 개수 T 입력
+	  N (1 <= N <= 100) 입력
+output P(N) 출력
+
+case 1 [2 6 12] [3 16]
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//overflow..
+long long cache[100] = {1, 1, 1, 2, 2, 0, };
+
+long long P(int N){
+	if(N <= 5) return cache[N-1];
+	
+	long long &ret = cache[N-1];
+	if(ret != 0) return ret;
+	
+	return ret = P(N-5) + P(N-1);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int T, N;
+	
+	cin >> T;
+	while(T-->0){
+		cin >> N;
+		cout << P(N) << "\n";
+	}
+	
+	return 0;
+}
